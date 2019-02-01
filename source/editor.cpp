3c342e768eaacf7d99684032b71de6c6aee94c76
Sikny <pamartqu@gmail.com>
2019-02-01 15:29:16 +0100

    Old style casts replaced by static_cast

--- source/editor.cpp
+++ source/editor.cpp
@@ -94,8 +94,8 @@ void Editor::lineNumberAreaPaintEvent(QPaintEvent *event)
     painter.fillRect(event->rect(), Theme::Instance().color("currentline"));
     QTextBlock block = firstVisibleBlock();
     int blockNumber = block.blockNumber();
-    int top = (int) blockBoundingGeometry(block).translated(contentOffset()).top();
-    int bottom = top + (int) blockBoundingRect(block).height();
+    int top = static_cast<int>(blockBoundingGeometry(block).translated(contentOffset()).top());
+    int bottom = top + static_cast<int>(blockBoundingRect(block).height());
     while (block.isValid() && top <= event->rect().bottom()) {
             if (block.isVisible() && bottom >= event->rect().top()) {
                 QString number = QString::number(blockNumber + 1) + " ";
@@ -106,7 +106,7 @@ void Editor::lineNumberAreaPaintEvent(QPaintEvent *event)
 
             block = block.next();
             top = bottom;
-            bottom = top + (int) blockBoundingRect(block).height();
+            bottom = top + static_cast<int>(blockBoundingRect(block).height());
             ++blockNumber;
     }
 }
